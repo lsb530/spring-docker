75f56bef505e5d90fdc8f62f7b6ca2346d6a7dfd
seung.bok <seungbok.lee@huinnoaim.com>
2023-09-25 16:10:03 +0900

    13.5차: prod 환경 nginx proxy 실패중..ㅠ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,8 @@ function App() {
     // 컴포넌트가 마운트 될 때 API를 호출합니다.
   useEffect(() => {
     // API 호출
-    axios.get('http://localhost:8080/api')
+    // axios.get('http://localhost:8080/api')
+    axios.get('/api')
         .then((res) => {
           console.log(res.data);
           setApiResult(res.data); // API 결과를 state에 저장합니다.
@@ -20,7 +21,8 @@ function App() {
 
   useEffect(() => {
     // API 호출
-    axios.get('http://localhost:8080/cors')
+    // axios.get('http://localhost:8080/cors')
+    axios.get('/cors') // => CORS 호출이 일어나서 막히길 기대. But Http Status: 200, App.tsx 로 리다이렉트됨
         .then((res) => {
           console.log(res.data);
         })
@@ -29,7 +31,8 @@ function App() {
 
   useEffect(() => {
     // API 호출
-    axios.get('http://localhost:8080/api/users')
+    axios.get('http://localhost:8080/api/users') // localhost로 요청하면 안됨
+    // axios.get('/api/users')
         .then((res) => console.log(res.data))
         .catch(err => console.error(err)); // 에러 핸들링
   }, []); // 빈 배열을 dependency로 전달하여 한 번만 실행하게 합니다.
